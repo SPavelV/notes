5cbc9669848bb4f0ac1f3d42dffb5853641194f7
Pavel Solovev <p.solovev@articul.ru>
2022-11-08 20:08:09 +0400

    User requests

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -2,5 +2,38 @@ module.exports = {
   notes: async (parent, args, { models }) => await models.Note.find(),
   note: async (parent, args, { models }) => {
     return await models.Note.findById(args.id);
+  },
+  user: async (parent, { username }, { models }) => {
+    return await models.User.findOne({ username });
+  },
+  users: async (parent, args, { models }) => {
+    return await models.User.find({});
+  },
+  me: async (parent, args, { models, user }) => {
+    return await models.User.findById(user.id);
   }
 };
+
+// query {
+//   user(username: 'adam') {
+//     username
+//     email
+//     id
+//   }
+// }
+
+// query {
+//   users {
+//     username
+//     email
+//     id
+//   }
+// }
+
+// query {
+//   me {
+//     username
+//     email
+//     id
+//   }
+// }
